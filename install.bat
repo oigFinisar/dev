8f7fd7e7dc0762ed5dbe4a89f8d3ad858e76f858
Andrei Valdez <andrei.valdez@finisar.com>
2025-07-30 14:24:43 +1000

    Fix bug in install batch file

--- install.bat
+++ install.bat
@@ -1,3 +1,2 @@
-REM http://syd-mfg-confluence.syd.ads.finisar.com:8090/x/xUdZDQ
-gem install jekyll bundler
-gem install jekyll-theme-slate
+@REM http://syd-mfg-confluence.syd.ads.finisar.com:8090/x/xUdZDQ
+gem install jekyll bundler jekyll-theme-slate
